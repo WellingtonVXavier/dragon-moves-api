fefa16d30564f5a68645a776f7a37613320c3686
Wellington Vaz Xavier <toomvxavier@outlook.com>
2024-06-11 12:39:14 -0300

    criando pastas de classes

---
+++ Megalopolis/Model/Dto/FilmesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Megalopolis.Model.Dto
+{
+    public class FilmesDto
+    {
+        public string Image { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ Megalopolis/Model/Filmes.cs
@@ -0,0 +1,14 @@
+﻿using Megalopolis.Base;
+
+namespace Megalopolis.Model
+{
+    public class Filmes: BaseModel
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public DateTime DataEncerramento { get; set; }
+        public string Duracao { get; set; }
+        public string image { get; set; }
+    }
+}
